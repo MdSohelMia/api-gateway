79a308fcf7654d3d0477e51bef1668e8be24517e
adisakshya <adisakshya98@gmail.com>
2021-12-18 16:56:13 +0530

    added docker-compose for single-node api-gateway

--- app-service-a/app.py
+++ app-service-a/app.py
@@ -4,7 +4,7 @@ application = app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World! This Application server 'A'."
+    return "Hello World! This is Application server 'A'."
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
